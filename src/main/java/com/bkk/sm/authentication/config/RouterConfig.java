68add504b41b7ba1f9bff21e3a9558406a2c06a1
davidk76 <mikehammer1902@gmail.com>
2022-04-04 00:30:26 +0100

    SM-

---
+++ src/main/java/com/bkk/sm/authentication/config/RouterConfig.java
@@ -0,0 +1,20 @@
+package com.bkk.sm.authentication.config;
+
+import com.bkk.sm.authentication.handler.LoginHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
+
+@Configuration
+public class RouterConfig {
+
+    @Bean
+    public RouterFunction<ServerResponse> loginRoutes(LoginHandler loginHandler) {
+        return RouterFunctions
+                .route(POST("/login"), loginHandler::login);
+    }
+}
