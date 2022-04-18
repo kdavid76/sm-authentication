6373cf19c03d2818fde1c6b004b8888ace857ebe
davidk76 <mikehammer1902@gmail.com>
2022-04-18 23:19:30 +0100

    SM: Little bit of upgrading the Auth process

--- src/main/java/com/bkk/sm/authentication/services/mongo/UserDetailsService.java
+++
@@ -1,6 +0,0 @@
-package com.bkk.sm.authentication.services.mongo;
-
-import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
-
-public interface UserDetailsService extends ReactiveUserDetailsService {
-}
